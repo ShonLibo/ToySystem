7dfe39eaf2630b8c7a23b8241a688c149d364ce7
ShonLibo <Benjaminlibo222@gmail.com>
2024-10-27 22:25:06 +0300

    Commit1
    In line 9 of the main class on the old version, Football was replaced with Volly

--- Main.java
+++ Main.java
@@ -6,7 +6,7 @@ public class Main {
         Toy[] toys = {
                 new Car("Toy Car", "A fast red car", "Green", 320),
                 new Doll("Barbie doll", "A beautiful doll", "Black", "white"),
-                new Ball("Football", "A big bouncy ball", 20, "White")
+                new Ball("Volly", "A big bouncy ball", 20, "White")
         };
 
         System.out.println("Welcome to the Toy Box! Here are the toys:");
