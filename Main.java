f71b09a784e2db9cdaf927cb068923218f426628
ShonLibo <Benjaminlibo222@gmail.com>
2024-10-27 21:54:08 +0300

    Commit 16
    In Line 9 of the main class in the old version, Football had the color Yellow. In the new version, it is color Black.

--- Main.java
+++ Main.java
@@ -6,7 +6,7 @@ public class Main {
         Toy[] toys = {
                 new Car("Toy Car", "A fast red car", "Green", 100),
                 new Doll("Barbie doll", "A beautiful doll", "Black", "white"),
-                new Ball("Football", "A big bouncy ball", 9, "Yellow")
+                new Ball("Football", "A big bouncy ball", 9, "Black")
         };
 
         System.out.println("Welcome to the Toy Box! Here are the toys:");
